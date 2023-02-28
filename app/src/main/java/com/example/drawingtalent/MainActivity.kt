0632c8400a6982499174dfa47f520077900b0c77
Andreea <totyo_deea@yahoo.com>
2023-02-28 14:50:13 -0500

    color pallet added

--- app/src/main/java/com/example/drawingtalent/MainActivity.kt
+++ app/src/main/java/com/example/drawingtalent/MainActivity.kt
@@ -4,10 +4,14 @@ import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView : DrawingView? = null
+    private var mImageButtonCurrentPaint : ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +19,13 @@ class MainActivity : AppCompatActivity() {
 
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
+
+        val linerLayoutPaintColors = findViewById<LinearLayout>(R.id.ll_paint_colors)
+        //use the colors in the linear layout
+        mImageButtonCurrentPaint = linerLayoutPaintColors[2] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+        )
 
         val ib_brush : ImageButton = findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener{
