4cb16053859fc2f3c5349dd3817edf6eb88c1969
Andreea <totyo_deea@yahoo.com>
2023-03-30 17:33:01 -0400

    save image on the device

--- app/src/main/java/com/example/drawingtalent/MainActivity.kt
+++ app/src/main/java/com/example/drawingtalent/MainActivity.kt
@@ -4,10 +4,15 @@ import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -16,7 +21,15 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.createBitmap
 import androidx.core.view.get
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -88,6 +101,19 @@ class MainActivity : AppCompatActivity() {
             drawingView?.onClickUndo()
         }
 
+        val ib_save : ImageButton = findViewById(R.id.ib_save)
+        ib_save.setOnClickListener{
+          // verify permission to write to external storage
+            if(isReadStorageAllowed()){
+                lifecycleScope.launch{
+                    val flDrawingView: FrameLayout = findViewById(R.id.fl_drawing_view_container)
+                    saveBitmapFile(getBitmapFromView(flDrawingView))
+                }
+            }
+
+        }
+
+
         val ib_redo : ImageButton = findViewById(R.id.ib_redo)
         ib_redo.setOnClickListener{
             drawingView?.onClickRedo()
@@ -97,6 +123,64 @@ class MainActivity : AppCompatActivity() {
         ib_gallery.setOnClickListener {
             requestStoragePermission()
         }
+    }
+   private suspend fun saveBitmapFile(mBitmap: Bitmap?) : String {
+        var result = ""
+        withContext(Dispatchers.IO) {
+            if(mBitmap != null) {
+                try{
+                    val bytes = ByteArrayOutputStream()
+                    mBitmap.compress(Bitmap.CompressFormat.PNG, 90 ,bytes)
+
+                    val f = File(externalCacheDir?.absoluteFile.toString() +
+                            File.separator + "DrawingTalent" + System.currentTimeMillis() /1000
+                            + ".png")
+
+                    val fo = FileOutputStream(f)
+                    fo.write(bytes.toByteArray())
+                    fo.close()
+
+                    result = f.absolutePath
+
+                    runOnUiThread{
+                        if(result.isNotEmpty()){
+                            Toast.makeText(
+                                this@MainActivity,
+                                "File saved successfully : $result",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Something went wrong while saving the file",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                } catch (e: java.lang.Exception){
+                    result = ""
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result
+    }
+
+    private fun getBitmapFromView(view: View) : Bitmap {
+        val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+
+        if(bgDrawable != null){
+            bgDrawable.draw(canvas)
+        } else {
+            canvas.drawColor(Color.WHITE)
+        }
+
+        view.draw(canvas)
+
+        return returnedBitmap
     }
     private fun showBrushSizeChooserDialog(){
         val brushDialog = Dialog(this)
@@ -140,6 +224,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun isReadStorageAllowed() : Boolean {
+        val result = ContextCompat.checkSelfPermission(this,
+        Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
     private fun requestStoragePermission(){
         if(ActivityCompat.shouldShowRequestPermissionRationale(
                 this,
@@ -149,8 +240,8 @@ class MainActivity : AppCompatActivity() {
                 "DrawingTalent App needs to Access Your External Storage")
         } else {
             requestPermission.launch(arrayOf(
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            //TODO - Add writing external storage
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
             ))
         }
     }
